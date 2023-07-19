3a083395b223e046072d59be714d56135a4b4b54
Soniah-13 <luteresoniah@gmail.com>
2023-07-19 02:30:45 -0700

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,53 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdlib.h>
+/**
+ * op_add - add function
+ * @a: input one
+ * @b: input two
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtraction function
+ * @a: input one
+ * @b: input two
+ * Return: defference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul -Multiplication
+ * @a: input one
+ * @b: input two
+ * Return: Product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Division
+ * @a: input one
+ * @b: input two
+ * Return: Divided numbers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod -  Modulas
+ * @a: input one
+ * @b: input two
+ * Return: Remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
